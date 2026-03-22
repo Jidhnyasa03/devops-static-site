3f5b901c5899a3473d58ac52144d75b23ea69400
Jidhnyasa03 <kulkarnijidhnyasa@gmail.com>
2026-03-22 13:42:53 +0530

    Initial devops project setup

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region"      { default = "ap-south-1" }
+variable "ami_id"          { default = "ami-0f58b397bc5c1f2e8" }
+variable "bucket_name"     { default = "jidhnyasa-devops-site-2025" }
+variable "public_key_path" { default = "C:/Users/Jidhnyasa/.ssh/devops-key.pub" }
